9913cd2dcd2601516dd2b9758d55672352479442
Mauro <mauromaio.dev@gmail.com>
2025-03-10 19:44:14 -0300

    fix: correção line do polygon

--- containers/Greetings.tsx
+++ containers/Greetings.tsx
@@ -4,7 +4,6 @@ import SocialLinks from '../components/SocialLinks';
 import { greetings } from '../portfolio';
 import { GithubUserType } from '../types';
 
-// Modificando o componente para aceitar o githubProfileData como prop
 const Greetings = ({
   githubProfileData,
 }: {
@@ -78,21 +77,7 @@ const Greetings = ({
               </Row>
             </div>
           </Container>
-          {/* SVG separator */}
-          <div className="separator separator-bottom separator-skew">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon className="fill-white" points="2560 0 2560 100 0 100" />
-            </svg>
-          </div>
         </section>
-        {/* 1st Hero Variation */}
       </div>
     </main>
   );
